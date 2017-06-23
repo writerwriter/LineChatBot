e76f985681847adf33f1decdc4c5b480d6985c5e
Wr1t3R <catchingfire80@gmail.com>
2017-06-23 22:46:50 +0800

    add if

--- app_run.py
+++ app_run.py
@@ -33,10 +33,10 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+    if "idiot" in event.message.text:
+        line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
