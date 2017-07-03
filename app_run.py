46b63eeec6cdad4443754234dc6cd1fe1b168b3f
Wr1t3R <catchingfire80@gmail.com>
2017-07-03 16:22:06 +0800

    maybe video will success

--- app_run.py
+++ app_run.py
@@ -52,10 +52,10 @@ def handle_message(event):
                 original_content_url='https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19400027_10155616239051564_8978737683901429880_n.jpg?oh=d6ead0d58f3359369b39cb50b4ee8ee7&oe=59CB836D', 
                 preview_image_url='https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19400027_10155616239051564_8978737683901429880_n.jpg?oh=d6ead0d58f3359369b39cb50b4ee8ee7&oe=59CB836D'
             ))
-    elif curMessage == "!writerwriter":
+    elif curMessage == "AdeleHello":
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text='https://www.youtube.com/embed/VQ6ch8Brpg4'))
+            VideoSendMessage(original_content_url='http://6056fa77.ngrok.io/video/AdeleHello.mp4', preview_image_url=None))
     elif curMessage == 'Sam':
         line_bot_api.reply_message(event.reply_token,
         TextSendMessage(text='Idiot'))
