f2c146b4b93a1711f22c112a460146593d274b2b
Wr1t3R <catchingfire80@gmail.com>
2017-07-03 21:09:40 +0800

    test

--- app_run.py
+++ app_run.py
@@ -55,7 +55,7 @@ def handle_message(event):
     elif curMessage == "AdeleHello":
         line_bot_api.reply_message(
             event.reply_token,
-            VideoSendMessage(original_content_url='https://c0f932f1.ngrok.io/video/trailer.mp4', preview_image_url='https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19400027_10155616239051564_8978737683901429880_n.jpg?oh=d6ead0d58f3359369b39cb50b4ee8ee7&oe=59CB836D'))
+            VideoSendMessage(original_content_url='http://4336f50f.ngrok.io/test.mp4', preview_image_url='https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19400027_10155616239051564_8978737683901429880_n.jpg?oh=d6ead0d58f3359369b39cb50b4ee8ee7&oe=59CB836D'))
     elif curMessage == 'Sam':
         line_bot_api.reply_message(event.reply_token,
         TextSendMessage(text='Idiot'))
